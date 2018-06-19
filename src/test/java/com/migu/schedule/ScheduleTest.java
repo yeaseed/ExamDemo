dd16b695e89553f9fe1895729d8db85f6e781866
yeaseed <cqkxgmzy@gmail.com>
2018-06-20 00:57:45 +0800

    1

--- src/test/java/com/migu/schedule/ScheduleTest.java
+++ src/test/java/com/migu/schedule/ScheduleTest.java
@@ -118,6 +118,7 @@ public class ScheduleTest
         actual = schedule.addTask(1, 10);
         actual = schedule.deleteTask(1);
         Assert.assertEquals(ReturnCodeKeys.E011, actual);
+        System.out.println(1);
     }
     
     @Test
